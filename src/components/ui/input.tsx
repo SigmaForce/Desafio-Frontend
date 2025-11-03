3d5e887437e071fc02bd62dabe786ce43d125c58
Leonardo Matos <leonardolucas2020@gmail.com>
2025-11-03 19:19:47 -0400

    :recycle: refactoring: add icons to input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,6 +3,11 @@ import { tv, type VariantProps } from "tailwind-variants";
 
 const inputVariants = tv({
   base: "p-3 w-full h-11 placeholder:text-[#6F6D78] border-[#3C393F] border bg-mauve-dark-950 rounded-sm focus:border-[#8E4EC6] outline-none text-[#EEEEF0]",
+  variants: {
+    hasIcon: {
+      true: "pr-10",
+    },
+  },
 });
 
 const containerVariants = tv({
@@ -24,13 +29,30 @@ interface InputProps
   extends ComponentProps<"input">,
     VariantProps<typeof containerVariants> {
   label?: string;
+  icon?: React.ReactNode;
 }
 
-export function Input({ label, className, variant, ...props }: InputProps) {
+export function Input({
+  label,
+  className,
+  variant,
+  icon,
+  ...props
+}: InputProps) {
   return (
     <div className={containerVariants({ variant })}>
       {label && <label className="font-bold text-xs">{label}</label>}
-      <input className={inputVariants({ className })} {...props} />
+      <div className="relative w-full">
+        <input
+          className={inputVariants({ className, hasIcon: !!icon })}
+          {...props}
+        />
+        {icon && (
+          <div className="absolute right-3 top-1/2 -translate-y-1/2 text-[#6F6D78] pointer-events-none">
+            {icon}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
